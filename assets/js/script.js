3832f4342efa59b7d3e04a5399b3a5345207dca6
Paul Pinard <paul.k.pinard@gmail.com>
2022-08-08 22:37:30 -0400

    js function for coloring time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@
 var dateEl = $('#currentDay');
 dateEl.text(moment().format('dddd, MMMM Do'))
 
+// Get current hour and format coloring for descriptions
+var hourNow = parseInt(moment().format('H'));
+var descriptionEls = $('.description');
+
+for (var i = 0; i < descriptionEls.length; i++) {
+    var descriptionHour = parseInt(descriptionEls[i].dataset.hour);
+
+    if (hourNow > descriptionHour) {
+        descriptionEls[i].classList.add('past');
+    } else if (hourNow === descriptionHour) {
+        descriptionEls[i].classList.add('present');
+    } else {
+        descriptionEls[i].classList.add('future');
+    }
+};
