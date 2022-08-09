77810eae0f0bca21bc03d1bf7e320b492251e58b
Paul Pinard <paul.k.pinard@gmail.com>
2022-08-08 23:50:53 -0400

    save button event listener attempt

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,14 @@ for (var i = 0; i < descriptionEls.length; i++) {
         descriptionEls[i].classList.add('future');
     }
 };
+
+// A function to store the content of the description in localStorage
+function storeAgendum(element) {
+    localStorage.setItem(element[0].id, element[0].value)
+}
+
+// Add an event listener to the save buttons
+var saveBtns = document.querySelectorAll('.saveBtn');
+for (var i = 0; i < saveBtns.length; i++) {
+    var partnerId = '#' + saveBtns[i].dataset.partner;
+}
