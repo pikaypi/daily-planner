3360248271d5fcfb355640663fa76e5416bb504d
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-04 13:46:15 -0400

    move content coloring into render function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,6 @@
 var dateEl = $('#currentDay');
 dateEl.text(moment().format('dddd, MMMM Do'))
 
-// Get current hour and format coloring for descriptions
-var hourNow = parseInt(moment().format('H'));
-var descriptionEls = $('.description');
-
-for (var i = 0; i < descriptionEls.length; i++) {
-    var descriptionHour = parseInt(descriptionEls[i].dataset.hour);
-
-    if (hourNow > descriptionHour) {
-        descriptionEls[i].classList.add('past');
-    } else if (hourNow === descriptionHour) {
-        descriptionEls[i].classList.add('present');
-    } else {
-        descriptionEls[i].classList.add('future');
-    }
-};
-
 // A function to store a saved agendum into local storage
 const saveAgendum = (event) => {
     var index;
@@ -31,18 +15,38 @@ const saveAgendum = (event) => {
     localStorage[inputSelected.id] = inputSelected.value
 };
 
+// A function to load the content from local storage
 const loadAgenda = () => {
+    // Check local storage for any saved data
     for (let i=9; i<18; i++) {
         if (localStorage[i]) {
             const inputSelected = document.getElementById(i);
             inputSelected.value = localStorage[i];
         }
-    }
+    };
+
+    // Get current hour and format coloring for descriptions
+    var hourNow = parseInt(moment().format('H'));
+    var descriptionEls = $('.description');
+
+    for (var i = 0; i < descriptionEls.length; i++) {
+        var descriptionHour = parseInt(descriptionEls[i].dataset.hour);
+
+        if (hourNow > descriptionHour) {
+            descriptionEls[i].classList.add('past');
+        } else if (hourNow === descriptionHour) {
+            descriptionEls[i].classList.add('present');
+        } else {
+            descriptionEls[i].classList.add('future');
+        }
+    };
 };
 
+// Add event listeners to the save buttons
 const saveBtns = document.querySelectorAll('.saveBtn');
 for (let i=0; i<saveBtns.length; i++) {   
     saveBtns[i].addEventListener('click', saveAgendum)
 };
 
+// Load the content every time the page reloads
 loadAgenda();
